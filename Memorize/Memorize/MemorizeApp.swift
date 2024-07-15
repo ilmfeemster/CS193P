f7ad6b081f1451588ce22c3c02de32000cfec21e
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2024-07-14 21:22:34 -0700

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Immanuel Matthews-Feemster on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
